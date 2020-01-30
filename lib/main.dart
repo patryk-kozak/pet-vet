a3b285ebe837bc8666502ebb3f7cd69fa0595a5a
pkozak-cl <pkozak@corelogic.com>
2020-01-30 23:30:04 +0100

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PetVet App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'New PetVet app!'),
     );
   }
 }
